97aeccfd4ba8e0366e76200c5bf2ad96b7cb4b44
Swmay <38032561+swmay@users.noreply.github.com>
2018-07-06 10:21:20 +0800

    Add a reminder
    A reminding to pop up when clicking on the function that the page does not provide.

--- static/js/sitemap.js
+++ static/js/sitemap.js
@@ -1,3 +1,79 @@
+/**
+* 弹出DIV空间覆盖页面的提醒浮层窗口，提醒内容为传入的str变量文本
+*/
+function Remind(str) {
+    var windowsWidth,windowsHight,borderColor;
+    windowsWidth=350;//提示窗口的宽度
+    windowsHight=80;//提示窗口的高度  
+    borderColor="#304558";//提示窗口的边框颜色
+    var sWidth,sHeight;
+    //获取当前窗口尺寸
+    sWidth = document.body.offsetWidth;
+    sHeight = document.body.offsetHeight;
+	//背景面板div
+    var remindPanel=document.createElement("div");
+    remindPanel.setAttribute('id','remindPanelID');
+    remindPanel.style.position="absolute";
+    remindPanel.style.top="0";
+    remindPanel.style.background="#E8E8E8";
+    remindPanel.style.filter="progid:DXImageTransform.Microsoft.Alpha(style=3,opacity=25,finishOpacity=75";
+    remindPanel.style.opacity="0.6";
+    remindPanel.style.left="0";
+    remindPanel.style.width = sWidth + "px";
+    remindPanel.style.height = sHeight + "px";
+    remindPanel.style.zIndex = "1";
+    document.body.appendChild(remindPanel);
+    //提示文本面板div
+    var remindTextPanel = document.createElement("div")
+    remindTextPanel.setAttribute("id","remindTextPanelID");
+    remindTextPanel.setAttribute("align","center");
+    remindTextPanel.style.background="white";
+    remindTextPanel.style.border="1px solid " + borderColor;
+    remindTextPanel.style.position = "absolute";
+    remindTextPanel.style.left = "50%";
+    remindTextPanel.style.font="12px/1.6em Verdana, Geneva, Arial, Helvetica, sans-serif";
+    //窗口距离左侧和顶端的距离
+    remindTextPanel.style.marginLeft = "-200px";
+    //窗口被卷去的高+（屏幕可用工作区高/2）-150
+    remindTextPanel.style.top = document.body.scrollTop+(window.screen.availHeight/2)-150 +"px";
+    remindTextPanel.style.width = windowsWidth + "px";
+    remindTextPanel.style.height = windowsHight + "px";
+    remindTextPanel.style.textAlign = "center";
+    remindTextPanel.style.lineHeight ="25px";
+    remindTextPanel.style.zIndex = "2";
+    document.body.appendChild(remindTextPanel);
+    //提示信息标题
+    var remindTitle=document.createElement("h4");
+    remindTitle.setAttribute("id","remindTitleText");
+    remindTitle.setAttribute("align","left");
+    remindTitle.style.margin="0";
+    remindTitle.style.padding="4px";
+    remindTitle.style.background = borderColor;
+    remindTitle.style.filter="progid:DXImageTransform.Microsoft.Alpha(startX=20, startY=20, finishX=100, finishY=100,style=1,opacity=75,finishOpacity=100);";
+    remindTitle.style.opacity="0.75";
+    remindTitle.style.border="1px solid " + borderColor;
+    remindTitle.style.height="18px";
+    remindTitle.style.font="12px Verdana, Geneva, Arial, Helvetica, sans-serif";
+    remindTitle.style.color="white";
+    remindTitle.innerHTML="MALFUNCTION REMINDING";
+    document.getElementById("remindTextPanelID").appendChild(remindTitle);
+    //提示信息
+    var remindText = document.createElement("p");
+    remindText.setAttribute("id","msgTxt");
+    remindText.style.margin="16px 0";
+    remindText.innerHTML = str; //将传入的提醒文本赋值到当前提醒中
+    document.getElementById("remindTextPanelID").appendChild(remindText);
+    //设置关闭时间
+    window.setTimeout("closewin()",2000);
+}
+function closewin() {
+    document.body.removeChild(document.getElementById("remindPanelID"));
+    document.getElementById("remindTextPanelID").removeChild(document.getElementById("remindTitleText"));
+    document.body.removeChild(document.getElementById("remindTextPanelID"));
+	  window.location.href="/";
+}
+
+
 /*
  *   对应<script>love(document.getElementById('love'));</script>脚本
  * */
